00c6b9415a78835d086ce487f96c3b300b049848
Federico Tavares <federicotavares19@gmail.com>
2025-04-18 20:09:28 -0300

    Agrego constructor de recurso

--- Domain/Recurso.cs
+++ Domain/Recurso.cs
@@ -5,4 +5,11 @@ public class Recurso
     public string nombre { get; set; }
     public string tipo { get; set; }
     public string descripcion { get; set; }
+
+    public Recurso(string nombre, string tipo, string descripcion)
+    {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+    }
 }
